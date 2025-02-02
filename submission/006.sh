e23edb72c4dd6018d27774dd369960431d69d9d5
gmc-b <gmcamargo.b@gmail.com>
2025-02-02 16:42:58 -0300

    exercício 006

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,5 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+TXID=$(bitcoin-cli getblock "$(bitcoin-cli getblockhash 256128)" 2 | jq -r '.tx[0].txid' )
+bitcoin-cli getblock "$(bitcoin-cli getblockhash 257343)" 2 |jq -r '.tx[] | select(any(.vin[].txid; . == "'$TXID'")) | .txid'
+
