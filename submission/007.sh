8d5f1eaf4c654d8ee6e1a2a57b5ded963310adb8
gmc-b <gmcamargo.b@gmail.com>
2025-02-02 23:19:38 -0300

    007 sem graça

--- submission/007.sh
+++ submission/007.sh
@@ -2,30 +2,9 @@
 
 BLOCK_NUMBER=123321
 
-
-
 CURRENT_BLOCK=$(bitcoin-cli getblock "$(bitcoin-cli getblockhash $BLOCK_NUMBER )" 2)
 mapfile -t TX_LIST < <(  echo $CURRENT_BLOCK | jq -r '.tx[] | "\(.txid) \(.vout[].n)"'  )
 
-
-
-while ((${#TX_LIST[@]} > 1 )); do
-    NEXT_BH=$(echo $CURRENT_BLOCK | jq -r '.nextblockhash')
-    CURRENT_BLOCK=$(bitcoin-cli getblock $NEXT_BH 2)
-
-    mapfile -t NEW_TX_LIST < <(echo $CURRENT_BLOCK | jq -r '.tx[] | "\(.vin[].txid) \(.vin[].vout)"' )
-    for i in ${!TX_LIST[@]};do
-        for j in ${!NEW_TX_LIST[@]};do
-            if [ "${TX_LIST[$i]}" == "${NEW_TX_LIST[$j]}" ]; then
-                unset TX_LIST[$i] 
-               
-            fi 
-        done
-    done
-    unset NEW_TX_LIST
+for i in ${!TX_LIST[@]}; do
+    bitcoin-cli gettxout ${TX_LIST[$i]} false | jq -r '.scriptPubKey.address'
 done
-
-
-UTXO=( $(echo ${TX_LIST[@]}))
-
- bc getrawtransaction "${UTXO[0]}" true | jq -r ".vout[] | select(.n == ${UTXO[1]})| .scriptPubKey.address"
