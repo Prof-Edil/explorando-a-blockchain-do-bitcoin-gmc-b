32af27126ac9f75e1ea3bcf69e7c779c362c4c83
gmc-b <gmcamargo.b@gmail.com>
2025-02-02 21:09:38 -0300

    ex 007

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,31 @@
-# Only one single output remains unspent from block 123,321. What address was it sent to?
+# Only one single output remains unspent from block 123,321. What addrress was it sent to?
+
+BLOCK_NUMBER=123321
+
+
+
+CURRENT_BLOCK=$(bitcoin-cli getblock "$(bitcoin-cli getblockhash $BLOCK_NUMBER )" 2)
+mapfile -t TX_LIST < <(  echo $CURRENT_BLOCK | jq -r '.tx[] | "\(.txid) \(.vout[].n)"'  )
+
+
+
+while ((${#TX_LIST[@]} > 1 )); do
+    NEXT_BH=$(echo $CURRENT_BLOCK | jq -r '.nextblockhash')
+    CURRENT_BLOCK=$(bitcoin-cli getblock $NEXT_BH 2)
+
+    mapfile -t NEW_TX_LIST < <(echo $CURRENT_BLOCK | jq -r '.tx[] | "\(.vin[].txid) \(.vin[].vout)"' )
+    for i in ${!TX_LIST[@]};do
+        for j in ${!NEW_TX_LIST[@]};do
+            if [ "${TX_LIST[$i]}" == "${NEW_TX_LIST[$j]}" ]; then
+                unset TX_LIST[$i] 
+               
+            fi 
+        done
+    done
+    unset NEW_TX_LIST
+done
+
+
+UTXO=( $(echo ${TX_LIST[@]}))
+
+ bc getrawtransaction "${UTXO[0]}" true | jq -r ".vout[] | select(.n == ${UTXO[1]})| .scriptPubKey.address"
